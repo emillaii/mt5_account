bd179120b7dd5e8deb2e00582f4a4a736be602c4
emillaii <laikwoktai@gmail.com>
2024-02-17 00:29:20 +0800

    Add command related api.

---
+++ docker/constants.js
@@ -0,0 +1,7 @@
+const COMMAND_STATUS_COMPLETED = "Completed";
+const COMMAND_STATUS_PENDING = "Pending";
+
+module.exports = {
+    COMMAND_STATUS_COMPLETED,
+    COMMAND_STATUS_PENDING
+};
--- docker/server.js
+++ docker/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { v4: uuidv4, stringify } = require('uuid');
 const cors = require("cors");
 const app = express();
 const port = 3000;
@@ -8,8 +9,11 @@ const MAX_RECORDS_PER_ACCOUNT = 1000;
 var cache = {}; // For time series plot 
 var positionsCache = {}; // For storing the latest position
 var dealsCache = {}; // For storing the deal position
+var allDealsCache = {}; // For storing the deal position requested by client
+var commandCache = {}; // For storing the command send to MT5 account
 
 const { users, tokens } = require('./config');
+const { COMMAND_STATUS_COMPLETED, COMMAND_STATUS_PENDING } = require('./constants');
 
 app.use(cors());
 // Middleware to parse request bodies
@@ -19,7 +23,6 @@ app.use(bodyParser.json());
 
 // POST endpoint to ingest data
 app.post('/ingest', (req, res) => {
-    console.log('Received data:', req.body);
     var data = req.body;
     if (data.dummy) delete data.dummy;
     var now = new Date();
@@ -49,7 +52,6 @@ app.post('/ingest', (req, res) => {
 });
 
 app.post('/positionIngest', (req, res) => {
-    console.log('Received position data:', req.body);
     var data = req.body;
     if (data.dummy) delete data.dummy;
     var now = new Date();
@@ -132,17 +134,88 @@ app.get('/getDeals', (req, res) => {
 // Login API
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
-    console.log(req.body);
     if (users[username] && users[username] === password) {
         console.log(`User ${username} logged in successfully`);
         res.status(200).send({
-            token: tokens[username],
+            token: tokens[username], 
             app: 'app.html'
         }); // Redirect to index.html on successful login
     } else {
         res.status(401).send('Invalid credentials');
     }
 });
+
+app.post('/commandList', (req, res) => {
+    var data = req.body;
+    let account = data.account;
+    account = account.trim();
+    if (!account) {
+        res.status(400).send('Invalid inputs');
+    }
+    res.status(200).send(commandCache[account]);
+})
+
+app.get('/command', (req, res) => {
+    var data = req.body;
+    let account = data.account;
+    if (!account) {
+        res.status(400).send('Invalid inputs');
+    }
+    res.status(200).send({ data: commandCache[account] });
+})
+
+app.post('/command', (req, res) => {
+    var data = req.body;
+    let commandId = uuidv4();
+    let commandData = { ...data, id: commandId, status: COMMAND_STATUS_PENDING, time: new Date().getTime() };
+    let account = req.body.account;
+    if (account) {
+        if (!commandCache[account]) {
+            commandCache[account] = [];
+        }
+        commandCache[account].push(commandData);
+    } else {
+        res.status(401).send('Invalid data');
+    }
+    res.json({
+        message: 'Command Received and stored',
+        commandData: commandData
+    })
+})
+
+// POST endpoint to ingest data
+app.post('/deals', (req, res) => {
+    var data = req.body;
+
+    if (data.dummy) delete data.dummy;
+  
+    let account = data.account;
+    let id = data.id;
+    if (account) {
+        if (!allDealsCache[account]) {
+            allDealsCache[account] = [];
+        }
+        for (var i = 0; i < commandCache[account].length; i++) {
+            console.log("command: %s command_ack: %s", commandCache[account][i].id, id);
+            if (commandCache[account][i].id === id) {
+                commandCache[account][i].status = COMMAND_STATUS_COMPLETED;
+            }
+        }
+        if (data.deals) {
+            allDealsCache[account] = JSON.parse(data.deals);
+        }
+    }
+    res.status(200).send('Data received successfully');
+});
+
+app.get('/deals', (req, res) => {
+    var data = req.body;
+    let account = data.account;
+    if (!account) {
+        res.status(400).send('Invalid inputs');
+    }
+    res.status(200).send({ data: allDealsCache[account] });
+})
 
 // Start the server
 app.listen(port, () => {
